cbfdf27f6113e746afee8039386aedf247dc30cc
qianshe <cutoffother@aliyun.com>
2025-02-21 22:25:17 +0800

    feat:增加回首页的按钮（复杂问题简单化）

--- pages/user/user.js
+++ pages/user/user.js
@@ -45,4 +45,11 @@ Page({
       }
     })
   },
+  onButtonClick: function (e) {
+    // 将页面栈清空，跳转到首页
+    wx.clearStorageSync()
+    wx.redirectTo({
+      url: '/pages/index_home/index_home'
+    })
+  }
 })
